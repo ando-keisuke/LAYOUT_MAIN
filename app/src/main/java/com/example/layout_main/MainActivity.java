288459a90881d0068a4ee7282f7badbe051aa179
ANDO_KEISUKE <it242017@trident.ac.jp>
2024-12-19 23:32:48 +0900

    位置情報の権限リクエストを追加

--- app/src/main/java/com/example/layout_main/MainActivity.java
+++ app/src/main/java/com/example/layout_main/MainActivity.java
@@ -1,8 +1,12 @@
 package com.example.layout_main;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 
+import android.content.pm.PackageManager;
 import android.os.Bundle;
+import android.Manifest;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,6 +15,20 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        // 位置情報の権限を求める
+        int LOCATION_PERMISSION_REQUEST_CODE = 100;
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION)
+                != PackageManager.PERMISSION_GRANTED) {
+            // 権限がない場合、リクエストを送信
+            Log.d("debug","Request ACCESS_FINE_LOCATION");
+
+            ActivityCompat.requestPermissions(this,
+                    new String[]{Manifest.permission.ACCESS_FINE_LOCATION},
+                    LOCATION_PERMISSION_REQUEST_CODE);
+        } else {
+            // 権限がすでに付与されている場合
+            Log.d("debug", "ACCESS_FINE_LOCATION: Granted");
+        }
 
         MapView mapView = findViewById(R.id.mapView);
 
