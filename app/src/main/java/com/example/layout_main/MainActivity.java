cd87e14bf023a3aa5cd1478e647c7de695e0edbc
ANDO_KEISUKE <it242017@trident.ac.jp>
2024-12-19 22:19:39 +0900

    ボトムシートの初期化処理を追加

--- app/src/main/java/com/example/layout_main/MainActivity.java
+++ app/src/main/java/com/example/layout_main/MainActivity.java
@@ -10,5 +10,27 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        MapView mapView = findViewById(R.id.mapView);
+
+        MapboxMap mapboxMap = mapView.getMapboxMap();
+
+        mapboxMap.loadStyle("mapbox://styles/kei242017/cm4u0pmti003z01sm7k001a71");
+
+        // ---- ボトムシートの初期設定 ----
+        // BottomSheetの参照を取得
+        View sheet = findViewById(R.id.sheet);
+
+        // BottomSheetBehaviorを設定
+        BottomSheetBehavior<View> bottomSheetBehavior = BottomSheetBehavior.from(sheet);
+
+        // neckHeightに相当する設定
+        bottomSheetBehavior.setPeekHeight(60); // peekHeightで初期表示時の高さを指定
+
+        // BottomSheetの状態を設定
+        bottomSheetBehavior.setState(BottomSheetBehavior.STATE_HIDDEN);
+
+
     }
 }
